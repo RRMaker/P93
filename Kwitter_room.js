c7d2c5ca6fb7855135098e97a66420c0cea6c36e
RRMaker <83099525+RRMaker@users.noreply.github.com>
2021-10-23 09:33:48 -0500

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,26 @@
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCdlAQgObSYHgm78xFL3vX35QcAqIs1Clk",
+  
+    authDomain: "lets-chat-web-app-b7794.firebaseapp.com",
+  
+    projectId: "lets-chat-web-app-b7794",
+  
+    storageBucket: "lets-chat-web-app-b7794.appspot.com",
+  
+    messagingSenderId: "618173011767",
+  
+    appId: "1:618173011767:web:7fce8ba66f944f7f152cb2",
+  
+    measurementId: "G-FSSM8WLDKW"
+  
+  };
+  
+  
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+  
